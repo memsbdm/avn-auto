95f6dcb111c27ab1b59575db9164870e0c1623ce
Mems <mehmetbdm@outlook.fr>
2022-10-23 15:10:52 +0200

    corrections navbar

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -12,9 +12,16 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {
     const section = document.querySelector('section') as HTMLElement;
     const bgContainer = document.querySelector('.container__right') as HTMLElement;
+
+    for(let i=1; i<51; i++){
+      setTimeout(()=>{
+        bgContainer.style.backgroundImage = `url("../assets/landing/${i}.png")`
+      },i*20)
+    }
+
     section.addEventListener('mousemove', (event)=>{
       if (window.innerWidth >= 1000) {
-        let nbImg = Math.round(event.clientX / window.innerWidth * 100 / 2 + 1);
+        let nbImg = Math.round(Math.round(event.clientX / window.innerWidth * 100 / 2 + 1)/2);
         bgContainer.style.backgroundImage = `url("../assets/landing/${nbImg}.png")`
       }
       else {
